762ce84c939c74cb2f9ff79ffcb0671678a6fd30
letscook1 <letscook1@hotmail.com>
2019-04-17 18:36:51 -0400

    WOAH LOL

--- bamazon.sql
+++ bamazon.sql
@@ -1,49 +1,34 @@
-DROP TABLE IF EXISTS `departments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
 CREATE TABLE `departments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `department_name` varchar(250) NOT NULL,
   `over_head_costs` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) 
 
---
--- Dumping data for table `departments`
---
+
 
 LOCK TABLES `departments` WRITE;
 /*!40000 ALTER TABLE `departments` DISABLE KEYS */;
 INSERT INTO `departments` VALUES (1,'home automation','22000'),(2,'music','12400'),(3,'sporting equipment','15220'),(4,'produce','2000'),(5,'alex\'s new department','230');
-/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(250) NOT NULL,
   `department_id` int(11) DEFAULT NULL,
   `price` decimal(8,2) DEFAULT NULL,
   `stock_quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `department_id` (`department_id`),
-  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
+  PRIMARY KEY (id),
+  KEY department_id (department_id),
+  FOREIGN KEY (department_id), 
+  REFERENCES departments (id)
+);
 
 LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+
 INSERT INTO `products` VALUES (1,'Rega P25 Turntable',2,1500.99,14),(2,'Amazon Echo',1,129.00,414),(3,'Golden Ear Triton 2 Speakers (Pair)',2,2300.99,3),(4,'Shake Weight',3,22.95,997),(5,'Vortex Power Bat',3,39.00,53),(6,'Google Nest Thermostat',1,300.00,59),(7,'Sonos Play 5 Speaker',2,369.99,134),(8,'Basketball Pump',3,15.95,21),(9,'SnapAV OVRC Pro Hub',1,200.99,4),(10,'Tycho Epoch Vinyl in Red Marble',2,17.95,200),(11,'Google Home',1,129.99,12),(12,'bananas',4,4.00,49),(13,'Alex\'s New Product',2,9.99,23);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -63,8 +48,7 @@ CREATE TABLE `sales` (
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
 --
 -- Dumping data for table `sales`
